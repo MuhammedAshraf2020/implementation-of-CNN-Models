46a2b3daf21f74fe226655359ee20c3e2a287ee7
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-02-25 17:57:22 +0200

    Update LeNet_tf.py

--- LeNet/LeNet_tf.py
+++ LeNet/LeNet_tf.py
@@ -5,7 +5,7 @@ from keras.models import Sequential
 
 def LeNet_tf():
   model = Sequential()
-  model.add(Input(shape = (32 , 32 , 1) ))
+  model.add(Input(shape = (28 , 28 , 1) ))
   model.add(Conv2D(filters = 6 , kernel_size = (3 , 3) , strides = (1 , 1)  , activation = "relu"))
   model.add(AveragePooling2D(2 , 2))
   model.add(Conv2D(filters = 16 , kernel_size = (3 , 3) , strides = (1 , 1)  , activation = "relu"))
