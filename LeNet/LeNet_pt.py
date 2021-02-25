7f5698002a054583916587abd6ad7371270ade74
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-02-25 17:56:01 +0200

    Update LeNet_pt.py

--- LeNet/LeNet_pt.py
+++ LeNet/LeNet_pt.py
@@ -6,13 +6,13 @@ class LeNet_pt(nn.Module):
 	def __init__(self):
 		super(LeNet_pt , self).__init__()
 		self.ConvModel = nn.Sequential( 
-					nn.Conv2d(in_channels = 1 , out_channels  = 6 , kernel_size   = (5 , 5) , padding = (0 , 0) , stride = (1 , 1)),
+					nn.Conv2d(in_channels = 1 , out_channels  = 6 , kernel_size   = (3 , 3) , padding = (0 , 0) , stride = (1 , 1)),
 					nn.ReLU() ,
 					nn.AvgPool2d(kernel_size = (2 , 2) , stride = (2 , 2)) ,
-					nn.Conv2d(in_channels = 6 , out_channels  = 16 , kernel_size   = (5 , 5) , padding = (0 , 0) , stride = (1 , 1)),
+					nn.Conv2d(in_channels = 6 , out_channels  = 16 , kernel_size   = (3 , 3) , padding = (0 , 0) , stride = (1 , 1)),
 					nn.ReLU() ,
 					nn.AvgPool2d(kernel_size = (2 , 2) , stride = (2 , 2)) ,
-					nn.Conv2d(in_channels = 16 , out_channels = 120 , kernel_size = (5 , 5) , padding = (0 , 0) , stride = (1 , 1)) )
+					nn.Conv2d(in_channels = 16 , out_channels = 120 , kernel_size = (3 , 3) , padding = (0 , 0) , stride = (1 , 1)) )
 
 		self.DenseModel = nn.Sequential(
 					nn.Linear(120 , 84),
