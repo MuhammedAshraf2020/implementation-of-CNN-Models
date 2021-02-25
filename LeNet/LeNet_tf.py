6edd609df21a8217b807307364278bcb0b265c0b
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-02-25 15:03:07 +0200

    Update LeNet_tf.py

--- LeNet/LeNet_tf.py
+++ LeNet/LeNet_tf.py
@@ -3,16 +3,16 @@ from keras.layers import Conv2D , AveragePooling2D , Input ,  Dense , Flatten
 from keras.models import Sequential
 
 
-model = Sequential()
-model.add(Input(shape = (32 , 32 , 1) ))
-model.add(Conv2D(filters = 6 , kernel_size = (3 , 3) , strides = (1 , 1)  , activation = "relu"))
-model.add(AveragePooling2D(2 , 2))
-model.add(Conv2D(filters = 16 , kernel_size = (3 , 3) , strides = (1 , 1)  , activation = "relu"))
-model.add(AveragePooling2D(2 , 2))
-model.add(Conv2D(filters = 128 , kernel_size = (3 , 3) , strides = (1 , 1)  , activation = "relu"))
-model.add(Flatten())
-model.add(Dense(84 , activation = "relu"))
-model.add(Dense(10))
-
-
-model.compile(loss = "categorical_crossentropy" , metrics = ["accuracy"] , optimizer = "adam") 
+def LeNet()
+  model = Sequential()
+  model.add(Input(shape = (32 , 32 , 1) ))
+  model.add(Conv2D(filters = 6 , kernel_size = (5 , 5) , strides = (1 , 1)  , activation = "relu"))
+  model.add(AveragePooling2D(2 , 2))
+  model.add(Conv2D(filters = 16 , kernel_size = (5 , 5) , strides = (1 , 1)  , activation = "relu"))
+  model.add(AveragePooling2D(2 , 2))
+  model.add(Conv2D(filters = 128 , kernel_size = (5 , 5) , strides = (1 , 1)  , activation = "relu"))
+  model.add(Flatten())
+  model.add(Dense(84 , activation = "relu"))
+  model.add(Dense(10))
+  model.compile(loss = "categorical_crossentropy" , metrics = ["accuracy"] , optimizer = "adam") 
+  return model
