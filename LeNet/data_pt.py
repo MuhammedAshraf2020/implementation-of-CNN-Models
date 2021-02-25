66ccbec98d4839390d7170352e52dfd0536e1008
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-02-25 17:38:15 +0200

    Create data_pt.py

---
+++ LeNet/data_pt.py
@@ -0,0 +1,16 @@
+import torchvision
+import torchvision.transforms as transforms
+
+def data_pt( batch_size = 150):
+  trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transforms.ToTensor())
+  trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size,
+                                          shuffle=True, num_workers=2)
+
+  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transforms.ToTensor())
+
+  testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+  return trainloader , testloader
