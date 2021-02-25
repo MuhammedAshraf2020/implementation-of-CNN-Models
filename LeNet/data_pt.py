015b4dc969efefadd6ae375747d3aed6a5b44fcd
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-02-25 17:50:33 +0200

    Update data_pt.py

--- LeNet/data_pt.py
+++ LeNet/data_pt.py
@@ -3,12 +3,12 @@ import torchvision.transforms as transforms
 import torch
 
 def data_pt( batch_size = 150):
-  trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+  trainset = torchvision.datasets.MNIST(root='./data', train=True,
                                         download=True, transform=transforms.ToTensor())
   trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size,
                                           shuffle=True, num_workers=2)
 
-  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+  testset = torchvision.datasets.MNIST(root='./data', train=False,
                                        download=True, transform=transforms.ToTensor())
 
   testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
