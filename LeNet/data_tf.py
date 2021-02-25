4ce4eb1ce8f8208b7f54a8616959a1b9bb4bbf6f
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-02-25 17:52:58 +0200

    Update data_tf.py

--- LeNet/data_tf.py
+++ LeNet/data_tf.py
@@ -4,6 +4,8 @@ from keras.utils import to_categorical
 from keras.datasets.mnist import load_data
 def data_tf():
   (x_train , y_train) , (x_test , y_test) = load_data()
+  x_train = x_train.reshape(x_train.shape + (1,))
+  x_test  = x_test.reshape(x_test.shape + (1,))
   print("x train shape = " , x_train.shape)
   print("x test  shape = " , x_test.shape)
   y_train = to_categorical(y_train)
